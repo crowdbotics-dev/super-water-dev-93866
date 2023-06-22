224f6d4ece97c496d262ae76416c274d47769da5
Crowdbotics <engineering@crowdbotics.com>
2023-06-22 09:36:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-water-dev-93866",
+  url: "https://super-water-dev-93866.botics.co",
+  api: "https://super-water-dev-93866.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
